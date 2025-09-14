17c38daf3351471f8e67162157260c7ada9941d8
Monica Sahakyan <monicasahakyan@MacBook-Pro-Monica.local>
2025-09-14 17:13:32 +0400

    modified task1

--- ENV-1.2/task1.cpp
+++ ENV-1.2/task1.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 
+bool isInteger(const std::string& str) {
+    if (str.empty())
+        return false;
+
+    int start = 0;
+    if (str[0] == '-')
+        start = 1;
+
+    if (start == str.size())
+        return false;
+
+    for (int i = start; i < str.size(); ++i) {
+        if (!std::isdigit(str[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         std::cout << "Usage: " << argv[0] << " a b c\n";
         return 1;
+    }
+
+    for (int i = 1; i < 4; ++i) {
+        if (!isInteger(argv[i])) {
+            std::cerr << "Error: '" << argv[i] << "' is not a valid integer.\n";
+            return 1;
+        }
     }
 
     int a = std::atoi(argv[1]);
